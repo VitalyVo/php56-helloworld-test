10840ba13c1bfe6f7288027d662ab26c8d66e745
Voloshchenko Vitaly <violee7@gmail.com>
2019-08-22 09:56:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello! It is a second deployment!</p>'; ?> 
+ <?php echo '<p>Hello world! Deployment is ready!</p>'; ?> 
  </body>
 </html>
